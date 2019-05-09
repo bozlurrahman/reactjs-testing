d334aff51b6fdc001969d9d87a291dba77f3fb4a
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2019-05-09 14:46:23 +0600

    jsx example

---
+++ src/JSX.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+
+const name = 'Josh Perez';
+const element = <h1>Hello, {name}</h1>;
+
+function JSXtest() {
+  return (
+    element
+  );
+}
+
+export default JSXtest;
--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import ShoppingList from './ShoppingList';
 import * as serviceWorker from './serviceWorker';
 import MyElement from "./CreateElement";
+import JSXtest from "./JSX";
 
 // ReactDOM.render(<ShoppingList name="Mark" />, document.getElementById('root'));
 // ReactDOM.render(<App />, document.getElementById('root'));
 
-ReactDOM.render(<MyElement name="Bozlur"></MyElement>, document.getElementById('root'));
+// ReactDOM.render(<MyElement name="Bozlur"></MyElement>, document.getElementById('root'));
+
+ReactDOM.render(<JSXtest></JSXtest>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
