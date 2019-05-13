eb90cae525a8897e69879bb427c5f5f2d3d693be
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2019-05-13 09:55:16 +0600

    State and Lifecycle Example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import * as serviceWorker from './serviceWorker';
-import ComposingComponents from './ComposingComponents'
-
-ReactDOM.render(<ComposingComponents/>,document.getElementById('root'));
-
-serviceWorker.unregister();
+function tick() {
+    const element = (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {new Date().toLocaleTimeString()}.</h2>
+      </div>
+    );
+    ReactDOM.render(
+      element,
+      document.getElementById('root')
+    );
+  }
+  
+  setInterval(tick, 1000);
