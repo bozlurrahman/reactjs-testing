ac2b426a59b5b4c02fd635882e4bfcd2db32ad67
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2019-05-13 14:28:12 +0600

    Example toggle event

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function ActionLink() {
-    function handleClick(e) {
-        e.preventDefault();
-        console.log('The link was clicked.');
-    }
-
-    return (
-        <a href="#" onClick={handleClick}>
-            Click me
-      </a>
-    );
-}
+import Toggle from './Toggle';
 
 ReactDOM.render(
-    <ActionLink />,
+    <Toggle />,
     document.getElementById('root')
 );
