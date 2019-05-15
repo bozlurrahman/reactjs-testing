2d421f529c460738b640f5efbcbc6dc2ce0c99d0
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2019-05-15 12:20:08 +0600

    simple form with name submittion

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NameForm from './NameForm';
 
-function ListItem(props) {
-    // <li key={value.toString()}></li>
-    // Correct! There is no need to specify the key here:
-    return <li>{props.value}</li>;
-}
 
-function NumberList(props) {
-    const numbers = props.numbers;
-    const listItems = numbers.map((number) =>
-        // <ListItem value={number} />
-        // Correct! Key should be specified inside the array.
-        <ListItem key={number.toString()} value={number} />
-    );
-    return (
-        <ul>
-            {listItems}
-        </ul>
-    );
-}
 
-const numbers = [1, 2, 3, 4, 5];
 ReactDOM.render(
-    <NumberList numbers={numbers} />,
+    <NameForm />,
     document.getElementById('root')
 );
