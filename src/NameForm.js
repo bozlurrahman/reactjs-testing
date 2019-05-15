7b5c0bec5e154ebc4db4c6019db0a8009559e3af
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2019-05-15 12:29:41 +0600

    adding textarea with form

--- src/NameForm.js
+++ src/NameForm.js
@@ -3,29 +3,36 @@ import React from 'react'
 class NameForm extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { value: '' };
+        this.state = {
+            name: '',
+            essay: 'Please write an essay about your favorite DOM element.'
+        };
 
-        this.handleChange = this.handleChange.bind(this);
+        this.handleNameChange = this.handleNameChange.bind(this);
+        this.handleEssayChange = this.handleEssayChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(event) {
-        this.setState({ value: event.target.value });
+    handleNameChange(event) {
+        this.setState({ name: event.target.value });
+    }
+    handleEssayChange(event) {
+        this.setState({ essay: event.target.value });
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        console.log(this.state.name);
+        console.log(this.state.essay);
+        
         event.preventDefault();
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
-                <label>
-                    Name:
-            <input type="text" value={this.state.value} onChange={this.handleChange} />
-                </label>
-                <input type="submit" value="Submit" />
+                <label>Name: <input type="text" value={this.state.name} onChange={this.handleNameChange} /></label><br /><br />
+                <label>Essay: <textarea value={this.state.essay} onChange={this.handleEssayChange} rows="6" cols="30" /></label>
+                <br /><br /><input type="submit" value="Submit" />
             </form>
         );
     }
