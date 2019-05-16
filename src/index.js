73f87b5ab30d465317fdf8c015ebaa138a4883b9
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2019-05-16 12:20:57 +0600

    containment the child elements.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Calculator from './TempCalc';
 
-
-
-ReactDOM.render(
-    <Calculator />,
+function FancyBorder(props) {
+    return (
+      <div className={'FancyBorder FancyBorder-' + props.color}>
+        {props.children}
+      </div>
+    );
+  }
+  
+  function WelcomeDialog() {
+    return (
+      <FancyBorder color="blue">
+        <h1 className="Dialog-title">
+          Welcome
+        </h1>
+        <p className="Dialog-message">
+          Thank you for visiting our spacecraft!
+        </p>
+      </FancyBorder>
+    );
+  }
+  
+  ReactDOM.render(
+    <WelcomeDialog />,
     document.getElementById('root')
-);
+  );
+  
