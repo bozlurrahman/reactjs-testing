eb450110215e6f0ea47b28f671911e4fb585434b
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2019-05-16 14:03:28 +0600

    Specialization for title and message passing through component.

--- src/index.js
+++ src/index.js
@@ -1,29 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './css/css1.css';
 
-function FancyBorder(props) {
+function Contacts() {
+    return <div className="Contacts" />;
+}
+
+function Chat() {
+    return <div className="Chat" />;
+}
+
+function SplitPane(props) {
     return (
-      <div className={'FancyBorder FancyBorder-' + props.color}>
-        {props.children}
-      </div>
+        <div className="SplitPane">
+            <div className="SplitPane-left">
+                {props.left}
+            </div>
+            <div className="SplitPane-right">
+                {props.right}
+            </div>
+        </div>
     );
-  }
-  
-  function WelcomeDialog() {
+}
+
+function App() {
     return (
-      <FancyBorder color="blue">
-        <h1 className="Dialog-title">
-          Welcome
-        </h1>
-        <p className="Dialog-message">
-          Thank you for visiting our spacecraft!
-        </p>
-      </FancyBorder>
+        <SplitPane
+            left={
+                <Contacts />
+            }
+            right={
+                <Chat />
+            } />
     );
-  }
-  
-  ReactDOM.render(
-    <WelcomeDialog />,
+}
+
+
+ReactDOM.render(<
+    App />,
     document.getElementById('root')
-  );
-  
+);
