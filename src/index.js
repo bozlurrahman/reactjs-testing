f7c2d34bd993487c3d3f4bab9e14292301c7b11b
Bozlur Rahman <bozlurrahman.cmt@gmail.com>
2019-05-23 14:45:16 +0600

    claring flow testing.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Splitting from './Splitting';
-import './scss/scss1.scss';
-
-import './scripts/flow-testing';
 
 ReactDOM.render(
     <Splitting />,
